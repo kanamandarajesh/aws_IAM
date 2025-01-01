92aabeef92b37f7c9753383b7f9106a1d8793c21
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2025-01-01 23:23:37 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull dockerspacex/simple-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5050:5000 dockerspacex/simple-python-app:latest
+docker run -d -p 5000:5000 dockerspacex/simple-python-app:latest

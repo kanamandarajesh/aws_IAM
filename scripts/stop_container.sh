7206f597ed52dcd904d45987b76a13b6dfe5ebac
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2025-01-01 23:23:18 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,3 +3,26 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi"
+
+# Get the container ID of the running container
+containerID=$(docker ps -q)
+
+# Remove the container if it's running
+docker rm -f $containerID
+
+
+#set -e
+
+# Stop the running container (if any)
+#echo "Hi"
+
+# Get the container ID of the running container
+# containerID=$(docker ps -q)
+
+# If a container is running, stop and remove it
+# if [ -n "$containerID" ]; then
+#  echo "Stopping and removing container with ID: $containerID"
+#  docker rm -f $containerID
+# else
+#  echo "No running container found."
+# fi
